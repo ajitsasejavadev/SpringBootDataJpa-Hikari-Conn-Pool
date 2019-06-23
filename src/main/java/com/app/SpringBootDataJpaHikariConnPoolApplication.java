b77f4d46e1fe76be71298afb4a3410dcc2ec0485
ajitsase <34818235+ajitsase@users.noreply.github.com>
2019-06-23 15:31:32 +0530

    Application For Hikari Pooling connection.

---
+++ src/main/java/com/app/SpringBootDataJpaHikariConnPoolApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataJpaHikariConnPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataJpaHikariConnPoolApplication.class, args);
+	}
+
+}
